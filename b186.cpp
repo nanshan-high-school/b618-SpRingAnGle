5c8296b460eb4578a28cbfe0e78c5e78f5b2981f
SpRingAnGle <32165234+SpRingAnGle@users.noreply.github.com>
2018-04-20 23:29:17 +0800

    Update b186.cpp

--- b186.cpp
+++ b186.cpp
@@ -3,16 +3,13 @@ using namespace std;
 void Free(int, int, int);
 
 int main() {
-  char origin[999999999];
-  cout << "請輸入原始數目\n";
-  cin >> origin;
-  
-  int originCookie, originChocolate, originCake;
-  
-  for () { //不知道怎麼取
-    
+  int origin[3];
+  cout << "請輸入原始數目(中間各隔一隔空格)\n";
+  for (int i = 0; i < 3; i++) {
+    cin >> origin[i];
   }
   
+  int originCookie = origin[0], originChocolate = origin[1], originCake = origin[2];
   Free(originCookie, originChocolate, originCake);
   
   return 0;
@@ -20,12 +17,12 @@ int main() {
 void Free(int originCookie, int originChocolate, int originCake) {
   int finalCookie = originCookie, finalChocolate, finalCake = originCake;
   
-  if (originCookie % 10 == 0 || originCake % 2 == 0) {
-    finalCake = originCake;
-  } else if ((originCookie % 10) > (originCake % 2)) {
-    finalCake = originCake + originCake%2;
-  } else if ((originCookie % 10) < (originCake % 2)) {
-    finalCake = originCake + originCookie%10;
+  if (originCookie / 10 == 0 || originCake / 2 == 0) {
+    finalChocolate = originChocolate;
+  } else if ((originCookie / 10) > (originCake / 2)) {
+    finalChocolate = originChocolate + originCake/2;
+  } else if ((originCookie / 10) < (originCake / 2)) {
+    finalChocolate = originChocolate + originCookie/10;
   }
   
   cout << finalCookie << " " << finalChocolate << " " << finalCake;
